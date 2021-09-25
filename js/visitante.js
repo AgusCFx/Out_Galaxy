18e0b5baa87e8dd186d900a6ba524034bc3df2aa
AgusCFx <agusfx00@gmail.com>
2021-09-25 19:22:19 -0300

    JS proyecto final 2 upd 1

---
+++ js/visitante.js
@@ -0,0 +1,25 @@
+let btnSetNickName = document.getElementById('btnSetNickName');
+let setNickName = document.getElementById('setNickName');
+let nickName;
+
+btnSetNickName.addEventListener('click', (e) => {
+    e.preventDefault();
+    nickName = setNickName.value;
+    console.log(nickName);
+})
+
+// let btnSetNickName = document.getElementById('btnSetNickName');
+// let setNickName = document.getElementById('setNickName');
+// let user = {
+//     nickname: '',
+//     age: '',
+//     galaxy: ''
+// };
+
+// btnSetNickName.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     user.nickname = setNickName.value;
+//     console.log(nickName)
+// })
+// localStorage.setItem('user', JSON.stringify(user))
+// localStorage.getItem('user', JSON.parse(user))
