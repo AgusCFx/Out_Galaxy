d914b78fbcd71c12b936fbec5fc1b56a070519aa
AgusCFx <agusfx00@gmail.com>
2021-10-04 18:56:59 -0300

    JS proyecto final 3 updt 5

--- js/themes.js
+++ js/themes.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-    
     let styles = {
         style1: {
             border: "1px solid black",
@@ -46,33 +45,5 @@ $(document).ready(function(){
     if(localStorage.getItem('theme')){
         let localTheme = localStorage.getItem('theme');
         changeBodyTheme(localTheme)
-    }
-    
-    
+    } 
 });
-
-
-// function changeBodyTheme(prop){
-//         body_theme.classList.remove('body_normal_theme');
-//         body_theme.classList.remove('body_hack_theme');
-//         body_theme.classList.remove('body_blind_theme');
-//         for (let i = 0; i < gridUniv.length; i++){ gridUniv[i].style.border = styles[`${prop}`].border};
-//         for (let i = 0; i < gridUniv__1.length; i++){ gridUniv__1[i].style.color = styles[`${prop}`].color};
-//         body_theme.classList.add(styles[`${prop}`].theme);
-//         localStorage.setItem("theme", prop)
-// }
-
-// let blind_theme = document.getElementById('blind_theme').addEventListener('click', (e) => {
-//     e.preventDefault();
-//     changeBodyTheme("style1");
-// })
-
-// let normal_theme = document.getElementById('normal_theme').addEventListener('click', (e) => {
-//     e.preventDefault();
-//     changeBodyTheme("style2");
-// })
-
-// let hack_theme = document.getElementById('hack_theme').addEventListener('click', (e) => {
-//     e.preventDefault();
-//     changeBodyTheme("style3")
-// })
